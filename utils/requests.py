dbb129689662de4d950b509d6dbcb48cc2c51a06
Ратмир Сарбасов <ratmir2020.1@gmail.com>
2025-01-17 12:21:39 +0500

    добавил файл requests.py для создания запросов

---
+++ utils/requests.py
@@ -0,0 +1,33 @@
+import requests
+
+
+def get_headers(token: str) -> dict[str, str]:
+    headers = {
+        "Authorization": "OAuth {token}".format(token=token),
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    }
+
+    return headers
+
+
+def make_request(
+    url: str,
+    token: str,
+    method: str,
+    params: dict = None,
+) -> requests.Response | dict[str, str]:
+    method = method.upper()
+
+    if method == "GET":
+        response = requests.get(url, headers=get_headers(token=token), params=params)
+    elif method == "PUT":
+        response = requests.put(url, headers=get_headers(token=token), params=params)
+    elif method == "POST":
+        response = requests.post(url, headers=get_headers(token=token), params=params)
+    elif method == "DELETE":
+        response = requests.delete(url, headers=get_headers(token=token), params=params)
+    else:
+        return {"error_message": "Указан неверный HTTP метод."}
+
+    return response
