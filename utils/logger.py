a2022d71ec9c39df3de73b00da36489208d03bbe
Ратмир Сарбасов <ratmir2020.1@gmail.com>
2025-01-19 10:48:28 +0500

    Изменил логи

--- utils/logger.py
+++ utils/logger.py
@@ -1,15 +1,23 @@
+import sys
+
 from loguru import logger
 
-# logger.add(
-#     sys.stdout,
-#     format="synchronizer {time} {level} {message}",
-#     level="INFO",
-# )
+logger.remove()
+
 
 logger.add(
-    "log.txt",
+    "logfile.log",
     format="synchronizer {time} {level} {message}",
     level="INFO",
+    backtrace=False,
+)
+
+
+logger.add(
+    sys.stdout,
+    format="synchronizer <green>{time}</green> <level>{level}</level> <cyan>{message}</cyan>",
+    level="INFO",
+    colorize=True,
 )
 
 
